8831baa8db3991ff35ed3a1dca3ef4d6b16168dd
Thomas Stephenson <ovangle@gmail.com>
2018-12-12 04:52:21 -0800

    header template is now not inlined

--- src/app/core/container/header/header.component.ts
+++ src/app/core/container/header/header.component.ts
@@ -1,15 +1,20 @@
 import {Component, ViewEncapsulation} from '@angular/core';
+import {createSelector, select, Store} from '@ngrx/store';
+import {UserState} from '../../../user/user.state';
+import {Observable} from 'rxjs';
+import {User} from '../../../user/user.model';
 
 
 @Component({
   selector: 'app-header',
-  template: `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand">Voter</a>
-      <ng-content></ng-content>
-    </nav>
-  `
+  templateUrl: './header.component.html'
 })
 export class AppHeaderComponent {
+  constructor(
+    readonly store: Store<any>
+  ) {}
 
+  readonly loginUser$: Observable<User | undefined> = this.store.pipe(
+    select(UserState.selectLoginUser)
+  );
 }
