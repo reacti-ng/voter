44edbb0dbf07a65ffca79c264a9a91c8e035d7b9
Thomas Stephenson <ovangle@gmail.com>
2018-12-12 04:33:16 -0800

    feat: Added guards to top level routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,26 @@
 import {Routes} from '@angular/router';
 import {coreRoutes} from './core/core.routes';
+import {IsAuthorizedGuard} from './common/auth/auth.guard';
 
 export const appRoutes: Routes = [
   ...coreRoutes,
   {
-    path: 'poll',
-    loadChildren: './poll/poll-feature.module#PollFeatureModule'
-  },
-  {
     path: 'org',
-    loadChildren: './org/org-feature.module#OrgFeatureModule'
+    loadChildren: './org/org-feature.module#OrgFeatureModule',
+    canActivateChild: [
+      IsAuthorizedGuard
+    ]
   },
   {
     path: 'proposal',
-    loadChildren: './proposal/proposal-feature.module#ProposalFeatureModule'
+    loadChildren: './proposal/proposal-feature.module#ProposalFeatureModule',
+    canActivateChild: [
+      IsAuthorizedGuard
+    ]
   },
   {
     path: 'login',
-    loadChildren: './user/features/login/login.module#UserLoginFeatureModule'
+    loadChildren: './user/features/login/login.module#UserLoginFeatureModule',
   },
   {
     path: 'public',
