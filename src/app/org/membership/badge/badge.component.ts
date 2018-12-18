587a3ed57e19dc2e781d59bf896ed92472a85692
Thomas Stephenson <ovangle@gmail.com>
2018-12-19 00:14:15 +1100

    refactor: move app-org-badge to app/shared/membership/badge

--- src/app/org/membership/badge/badge.component.ts
+++ src/app/org/membership/badge/badge.component.ts
@@ -1,15 +1,16 @@
 import {Component, Input} from '@angular/core';
+import {Org} from '../../../org.model';
 
 
 @Component({
-  selector: 'app-org-badge',
+  selector: 'app-org-membership-badge',
   templateUrl: './badge.component.html',
   styleUrls: [
     './badge.component.scss'
   ]
-
 })
 export class OrgBadgeComponent {
   @Input() size: 'xs' | 'sm' | 'md' | 'lg' = 'lg';
+  @Input() org: Org | undefined;;
 
 }
